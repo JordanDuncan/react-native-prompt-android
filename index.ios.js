550e78288997978f4205ababd8a75ceb57400f3c
Jordan Duncan <jrdndncn@gmail.com>
2018-07-10 15:04:32 +0100

    feat(index.ios): map type to type/keyboardType on iOS
    add function to return correct mapping of type/keyboardType for any supplied prompt type

--- index.ios.js
+++ index.ios.js
@@ -2,11 +2,64 @@ import {
     AlertIOS
 } from 'react-native';
 
+function convertToIosTypes(moduleType) {
+    switch (moduleType) {
+        case 'default':
+        return {
+            type: 'plain-text',
+            keyboardType: 'default'
+        };
+
+        case 'plain-text':
+        return {
+            type: 'plain-text',
+            keyboardType: 'default'
+        };
+
+        case 'secure-text':
+        return {
+            type: 'secure-text',
+            keyboardType: 'default'
+        };
+
+        case 'login-password':
+        return {
+            type: 'login-password',
+            keyboardType: 'default'
+        };
+
+        case 'numeric':
+        return {
+            type: 'plain-text',
+            keyboardType: 'numeric'
+        };
+        
+        case 'email-address':
+        return {
+            type: 'plain-text',
+            keyboardType: 'email-address'
+        };
+
+        case 'phone-pad':
+        return {
+            type: 'plain-text',
+            keyboardType: 'phone-pad'
+        };
+
+        default:
+        return {
+            type: 'plain-text',
+            keyboardType: 'default'
+        }
+    }
+}
+
 export default function prompt(
     title: ?string,
     message?: ?string,
     callbackOrButtons?: ?((text: string) => void) | Object,
     options?: Object
 ): void {
-    AlertIOS.prompt(title, message, callbackOrButtons, options.type, options.defaultValue);
+    let iosTypes = convertToIosTypes(options.type);
+    AlertIOS.prompt(title, message, callbackOrButtons, iosTypes.type, options.defaultValue, iosTypes.keyboardType);
 };
